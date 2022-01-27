4d012d7f9fb58de839b0e09578d258d82e34b55a
azenkin3 <93234779+azenkin3@users.noreply.github.com>
2022-01-27 11:15:07 +0900

    12345

--- js/ajax.js
+++ js/ajax.js
@@ -78,27 +78,54 @@ $.ajax({
                        
                 
                         }
-                    });
+                    }); 
+                    
 
                     $.ajax({
                         method: "GET",
                         url: "https://dapi.kakao.com/v3/search/book?target=title",
-                        data: { query: "글쓰기",size:50 },
-                       
-                        headers: { Authorization: "KakaoAK 236fe2947416052ad7044400f1de7e66" } 
-                    })
-                    
+                        data: { query: "월가의 퀀트" },
+                        headers: { Authorization: "KakaoAK c8ffc3ab6fd3e90abbd76c604e685526" }
+                      })
                         .done(function (msg) {
-                            let title5=$('.title5 article')
-                            for(i=0;i<title5.length;i++){
-                           
-                            $('.title5 article').eq(i).append("<img src='"  + msg.documents[i].thumbnail + "'/>"); 
-                            $('.title5 article').eq(i).append("<strong>" + msg.documents[i].title + "</strong> <br>"); 
-                            $('.title5 article').eq(i).append("<strong>" + msg.documents[i].price + "원</strong>");
-                           
+                          console.log(msg.documents[1].title);
+                          console.log(msg.documents[1].thumbnail);
+                
+                         
+                          
+                          $(".subtitle1").append("<img src='" + msg.documents[0].thumbnail + "'/>");
+                          $(".write").append("<h6>" + msg.documents[0].authors[0] + "저</h6> ");
+                         
+                          $(".pricenum").append(msg.documents[0].price + "원");
+                
+                      
                     
-                            }
-                        });
+                      $(function () {
+                        $.get("./sub_txt/txt1.txt", function (data) {
+                          $("#tmpBox").html(data);
+                        })
+                
+                        $.ajax({
+                                        method: "GET",
+                                        url: "https://dapi.kakao.com/v3/search/book?target=title",
+                                        data: { query: "글쓰기",size:50 },
+                                       
+                                        headers: { Authorization: "KakaoAK 236fe2947416052ad7044400f1de7e66" } 
+                                    })
+                                    
+                                        .done(function (msg) {
+                                            let title5=$('.title5 article')
+                                            for(i=0;i<title5.length;i++){
+                                           
+                                            $('.title5 article').eq(i).append("<img src='"  + msg.documents[i].thumbnail + "'/>"); 
+                                            $('.title5 article').eq(i).append("<strong>" + msg.documents[i].title + "</strong> <br>"); 
+                                            $('.title5 article').eq(i).append("<strong>" + msg.documents[i].price + "원</strong>");
+                                           
+                                    
+                                            }
+                                          });
+                      });
+                    });
                
             });
    
